9d29fc356f75888006695785a693fb13dbac0939
harshalpatil2809 <harshalpatil28095@gmail.com>
2025-10-15 23:28:18 +0530

    imgs added

--- script.js
+++ script.js
@@ -1,18 +1,1 @@
-const url = "https://api.thecatapi.com/v1/images/search?limit=50";
-let pic = document.createElement("img")
-function getdata() {
-  fetch(url)
-    .then((res) => {
-      return res.json();
-    })
-    .then((data) => {
-      for (let i = 0; i < data.length; i++) {
-        let photo_url = data[i].url
-        console.log(photo_url);
-        pic.src = photo_url
-        document.body.appendChild(pic)
-      }
-    });
-    
-}
-getdata();
+  
